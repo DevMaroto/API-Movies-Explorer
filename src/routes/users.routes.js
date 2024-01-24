4352e13de7fcc8ad3b3e6d3596ab324d95a2eb3c
Pedro Teodoro <pedropauloteodoro26@gmail.com>
2024-01-24 13:45:24 -0300

    auth configs

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,20 +1,29 @@
 // Criamos uma pasta com o nome de *routes* dentro da pasta *SRC* e é nela que ficará as rotas da aplicação, logo em seguida foi criado um arquivo (users.routes.js) tudo que for relacionado a rota de usuario ficará aqui)
 
- // foi retirado do arquivo server.js a rota de usuario que agora se chama usersRoutes
+// foi retirado do arquivo server.js a rota de usuario que agora se chama usersRoutes
 
 // importa de express o *Router*
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
-const UsersController = require("../controllers/UsersController")
+const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
-const usersRoutes = Router()
-
-
+const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/:id", usersController.update);
-
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  usersAvatarController.update
+);
 
 module.exports = usersRoutes;
